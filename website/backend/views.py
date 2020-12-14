b10213bcbbb770bc1434610029ee6ab6ff8bfafc
Benwaz78 <nonwaz78@gmail.com>
2020-12-14 13:19:46 +0100

    Save

--- website/backend/views.py
+++ website/backend/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def login_view(request):
